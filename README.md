579d5e529e6df4afa1a3bcdb71ebbb0a66f001f0
clc4217 <clc4217@ic.ac.uk>
2019-01-26 17:06:31 +0000

    it compiles

--- README.md
+++
@@ -1,3 +0,0 @@
-# Android Launcher
-
-TODO
---
+++ openlauncher/app/build/generated/source/buildConfig/flavorNightly/release/com/benny/openlauncher/BuildConfig.java
@@ -0,0 +1,21 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.benny.openlauncher;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.benny.openlauncher.nightly";
+  public static final String BUILD_TYPE = "release";
+  public static final String FLAVOR = "flavorNightly";
+  public static final int VERSION_CODE = 190126;
+  public static final String VERSION_NAME = "190126";
+  // Fields from product flavor: flavorNightly
+  public static final boolean IS_NIGHTLY_BUILD = true;
+  // Fields from default config.
+  public static final String BUILD_DATE = "2019-01-26T17:00Z";
+  public static final String[] DETECTED_ANDROID_LOCALES = {"zh-rCN","de","ru","nb-rNO","eo","it","fr","kab","hu","es","pt-rBR","cs","sc","ar","v21","ja","sk","pt-rPT","pl","ca","sr"};
+  public static final String GITHASH = "5832a7ff34b7c89a2eb26d9487cd6075609bb8b8";
+  public static final boolean IS_GPLAY_BUILD = false;
+  public static final boolean IS_TEST_BUILD = false;
+}
