96f1690abae7a50736aec32532a57a0fcb5982aa
clc4217 <clc4217@ic.ac.uk>
2019-01-26 17:46:49 +0000

    Forgot to add demos

--- openlauncher/app/build/generated/source/buildConfig/flavorDefault/debug/com/benny/openlauncher/BuildConfig.java
+++ openlauncher/app/build/generated/source/buildConfig/flavorDefault/debug/com/benny/openlauncher/BuildConfig.java
@@ -11,9 +11,9 @@ public final class BuildConfig {
   public static final int VERSION_CODE = 37;
   public static final String VERSION_NAME = "0.6.1";
   // Fields from default config.
-  public static final String BUILD_DATE = "2019-01-26T17:00Z";
+  public static final String BUILD_DATE = "2019-01-26T17:44Z";
   public static final String[] DETECTED_ANDROID_LOCALES = {"zh-rCN","de","ru","nb-rNO","eo","it","fr","kab","hu","es","pt-rBR","cs","sc","ar","v21","ja","sk","pt-rPT","pl","ca","sr"};
-  public static final String GITHASH = "5832a7ff34b7c89a2eb26d9487cd6075609bb8b8";
+  public static final String GITHASH = "579d5e529e6df4afa1a3bcdb71ebbb0a66f001f0";
   public static final boolean IS_GPLAY_BUILD = false;
   public static final boolean IS_TEST_BUILD = false;
 }
