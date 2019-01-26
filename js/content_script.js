c47dc6848683d5c24220780e6b9839793b45e1f4
Jakub Dranczewski <domdlapsa@gmail.com>
2019-01-26 19:19:43 +0000

    Basic interaction Accepts user input

--- js/content_script.js
+++ js/content_script.js
@@ -9,6 +9,22 @@ class MessageBox {
     }
 }
 
+function saveResponse() {
+    r_input.blur()
+    reply.classList.remove("why--reply-box")
+    send.classList.add("why--sent")
+    message1 = new MessageBox("from", "Ok, thanks!");
+    message1.div.style.animationName = "fade-in"
+    message1.div.style.animationDuration = "0.5s"
+    chat_window.appendChild(message1.div);
+    setTimeout(function() {
+        document.getElementById("why--container").classList.add("why--fadeout");
+        setTimeout(function() {
+            document.getElementById("why--container").style.display = "none";
+        }, 500)
+    }, 1000);
+}
+
 let container = document.createElement("div");
 container.id = "why--container";
 
@@ -24,12 +40,28 @@ chat_window.appendChild(br);
 reply = document.createElement("div");
 reply.classList.add("why--message-to");
 reply.classList.add("why--reply-box");
-reply.innerHTML = "Because I'm bored";
+
+r_input = document.createElement("input");
+r_input.type = "text"
+r_input.id = "why--reply-textarea";
+r_input.placeholder = "Because...";
+r_input.addEventListener('keyup', function (e) {
+    if (e.keyCode === 13) {
+        e.preventDefault();
+        saveResponse();
+    }
+});
+reply.appendChild(r_input);
+
 chat_window.appendChild(reply);
 
 send = document.createElement("img");
-send.id = "why--send"
+send.id = "why--send";
 send.src = chrome.runtime.getURL("images/send.svg")
+send.addEventListener('click', function(e) {
+    saveResponse();
+})
 chat_window.appendChild(send)
 
 body.appendChild(container);
+document.getElementById("why--reply-textarea").focus();
